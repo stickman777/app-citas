de0616008062ea80a09c0c86d9e246b509dc2062
Pablo <pcr00025@red.ujaen.es>
2026-04-24 13:31:09 +0200

    Roles de Admin/Cliente incorporados

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,19 +1,25 @@
+import { Roles } from '../auth/roles.decorator';
+import { RolesGuard } from '../auth/roles.guard';
+import { UserRole } from './user.entity';
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
 import { UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
-// Para proteger esta ruta, se usa el JwtAuthGuard, que verifica el JWT en la cabecera Authorization
-@UseGuards(JwtAuthGuard)
+
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles(UserRole.ADMIN)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
   // Endpoint para obtener todos los usuarios
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
+  
   // Endpoint para crear un nuevo usuario (registro) - se espera un JSON con email, password y rol
   @Post()
   create(@Body() userData: Partial<User>) {
