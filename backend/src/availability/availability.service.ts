31dc7c25f1e988a85d3019eae7e060dfcbbb9a9f
Pablo <pcr00025@red.ujaen.es>
2026-05-24 14:27:58 +0200

    Permitir varias franjas de disponibilidad sin solapes

--- backend/src/availability/availability.service.ts
+++ backend/src/availability/availability.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Availability } from './availability.entity';
@@ -19,7 +19,30 @@ export class AvailabilityService {
     });
   }
 
-  create(availabilityData: CreateAvailabilityDto) {
+  async create(availabilityData: CreateAvailabilityDto) {
+    if (availabilityData.startTime >= availabilityData.endTime)
+      throw new BadRequestException(
+        'La hora de inicio debe ser anterior a la hora de fin',
+      );
+
+    const availabilitiesSameDay = await this.availabilityRepository.find({
+      where: {
+        dayOfWeek: availabilityData.dayOfWeek,
+      },
+    });
+
+    const hasOverlap = availabilitiesSameDay.some((availability) => {
+      return (
+        availability.startTime < availabilityData.endTime &&
+        availability.endTime > availabilityData.startTime
+      );
+    });
+
+    if (hasOverlap)
+      throw new BadRequestException(
+        'La franja de disponibilidad se solapa con otra existente',
+      );
+
     const availability = this.availabilityRepository.create(availabilityData);
 
     return this.availabilityRepository.save(availability);
