fa7b4446bebc5252d425347e00929d4a62b416c3
Pablo <pcr00025@red.ujaen.es>
2026-05-25 21:07:24 +0200

    Modificaciones para política CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:4200', 'http://127.0.0.1:4200'],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       // Elimina propiedades no definidas en los DTOs
